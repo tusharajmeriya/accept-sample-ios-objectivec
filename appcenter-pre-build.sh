c026be85bde04462f3f4af8d5287f056d1ea560f
tusharajmeriya <tusharajmeriya@users.noreply.github.com>
2020-08-06 18:29:05 +0530

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,4 +2,8 @@
 
 echo "Executing the pre-build steps"
 #cd accept-sample-objc.xcodeproj
+
+### Clone Submodules
+git clone https://$BITBUCKET_USER:$BITBUCKET_PASSWORD@bitbucket.org/tusharajmeriya_infostretch/test_repo.git
+### Install SDK
 pod install
