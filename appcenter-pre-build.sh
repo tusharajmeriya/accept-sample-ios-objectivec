4441a0da84d44c64ba5444a084933093a43f45bc
tusharajmeriya <tusharajmeriya@users.noreply.github.com>
2020-08-06 19:39:19 +0530

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -7,7 +7,7 @@ echo "Executing the pre-build steps"
 git submodule update --recursive
 #git clone https://$BITBUCKET_USER:$BITBUCKET_PASSWORD@bitbucket.org/tusharajmeriya_infostretch/test_repo.git
 # Listing Test Repo
-ls test_repo
+ls submodules
 ls
 ### Install SDK
 pod install
